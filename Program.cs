528c7470677fd2a252a4638b3e8a2aab06efbab6
Latasha Simmons <lsimmons1832@gmail.com>
2017-07-22 14:02:03 -0500

    save progress

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQedList
 {
@@ -7,6 +9,26 @@ namespace LINQedList
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Restriction/Filtering Operations
+
+            // Find the words in the collection that start with the letter "L"
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            var LFruits = from fruit in fruits where fruit.First() == 'L' select fruit;
+
+            foreach (var fruit in LFruits)
+            {
+                Console.WriteLine(fruit); 
+            }
+
+            //Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var fourSixMultiples = numbers.Where(num => numbers % 4 = 0 || numbers % 6 = 0);
         }
     }
 }
