7ec6c81ecf9817ab7f1b9f2d5ce4640b6a26c121
Latasha Simmons <lsimmons1832@gmail.com>
2017-07-24 17:50:31 -0500

    save progress

--- Program.cs
+++ Program.cs
@@ -17,10 +17,10 @@ namespace LINQedList
 
             var LFruits = from fruit in fruits where fruit.First() == 'L' select fruit;
 
-            foreach (var fruit in LFruits)
-            {
-                Console.WriteLine(fruit); 
-            }
+            // foreach (var fruit in LFruits)
+            // {
+            //     Console.WriteLine(fruit);
+            // }
 
             //Which of the following numbers are multiples of 4 or 6
             List<int> numbers = new List<int>()
@@ -28,7 +28,35 @@ namespace LINQedList
                 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
             };
 
-            var fourSixMultiples = numbers.Where(num => numbers % 4 = 0 || numbers % 6 = 0);
+            var fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0).ToList();
+
+            // foreach (var num in numbers)
+            // {
+            //     Console.WriteLine(num);
+            // }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+                {
+                    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                    "Francisco", "Tre"
+                };
+
+                var descend = names.OrderByDescending(name => name);
+
+                // foreach (var name in descend)
+                // {
+                //     Console.WriteLine(name);
+                // }
+
+                // Build a collection of these numbers sorted in ascending order
+                // List<int> numbers = new List<int>()
+                // {
+                //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+                // }
         }
     }
 }
